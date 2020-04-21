f9794aca9c5a446106d4c71a7d1f42309f30603b
Michael Babker <michael.babker@gmail.com>
2020-04-20 23:04:18 -0500

    Contract the generator

---
+++ src/Contracts/BreadcrumbsGenerator.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace BabDev\Breadcrumbs\Contracts;
+
+use BabDev\Breadcrumbs\Exceptions\InvalidBreadcrumbException;
+use Illuminate\Support\Collection;
+
+interface BreadcrumbsGenerator
+{
+    /**
+     * Generate breadcrumbs.
+     *
+     * @param callable[] $callbacks The registered breadcrumb-generating closures.
+     * @param callable[] $before    The registered 'before' callbacks.
+     * @param callable[] $after     The registered 'after' callbacks.
+     * @param string     $name      The name of the current page.
+     * @param array      $params    The parameters to pass to the closure for the current page.
+     *
+     * @return Collection
+     *
+     * @throws InvalidBreadcrumbException if the name is (or any ancestor names are) not registered
+     */
+    public function generate(array $callbacks, array $before, array $after, string $name, array $params): Collection;
+
+    /**
+     * Add breadcrumbs for a parent page.
+     *
+     * Should be called from the closure for a page, before `push()` is called.
+     *
+     * @param string $name      The name of the parent page.
+     * @param array  ...$params The parameters to pass to the closure.
+     *
+     * @return void
+     *
+     * @throws InvalidBreadcrumbException if the name is (or any ancestor names are) not registered
+     */
+    public function parent(string $name, ...$params): void;
+
+    /**
+     * Add a breadcrumb.
+     *
+     * Should be called from the closure for each page. May be called more than once.
+     *
+     * @param string      $title The title of the page.
+     * @param string|null $url   The URL of the page.
+     * @param array       $data  Optional associative array of additional data to pass to the view.
+     *
+     * @return void
+     */
+    public function push(string $title, string $url = null, array $data = []): void;
+}
