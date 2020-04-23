7af5cd4d5427624ae652616443ed265a524f607d
Michael Babker <michael.babker@gmail.com>
2020-04-22 21:15:27 -0500

    Test the service provider in isolation

---
+++ tests/Providers/BreadcrumbsServiceProviderTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace BabDev\Breadcrumbs\Tests\Providers;
+
+use BabDev\Breadcrumbs\Contracts\BreadcrumbsGenerator;
+use BabDev\Breadcrumbs\Contracts\BreadcrumbsManager;
+use BabDev\Breadcrumbs\Providers\BreadcrumbsServiceProvider;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Orchestra\Testbench\TestCase;
+
+class BreadcrumbsServiceProviderTest extends TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [
+            BreadcrumbsServiceProvider::class,
+        ];
+    }
+
+    public function testProviderIsDeferred(): void
+    {
+        $this->assertTrue($this->app->getProvider(BreadcrumbsServiceProvider::class)->isDeferred());
+    }
+
+    public function testDeferredServicesAreListed(): void
+    {
+        $this->assertTrue(
+            in_array(
+                'breadcrumbs.generator',
+                $this->app->getProvider(BreadcrumbsServiceProvider::class)->provides()
+            )
+        );
+    }
+
+    public function testServicesAreBound(): void
+    {
+        $this->assertTrue($this->app->bound('breadcrumbs.generator'));
+        $this->assertInstanceOf(BreadcrumbsGenerator::class, $this->app->make('breadcrumbs.generator'));
+
+        $this->assertTrue($this->app->bound('breadcrumbs.manager'));
+        $this->assertInstanceOf(BreadcrumbsManager::class, $this->app->make('breadcrumbs.manager'));
+    }
+
+    public function testTheManagerIsResolvedWhenThereAreNoFiles(): void
+    {
+        $this->app['config']->set('breadcrumbs.files', []);
+
+        $this->assertInstanceOf(BreadcrumbsManager::class, $this->app->make('breadcrumbs.manager'));
+    }
+
+    public function testTheManagerIsResolvedWhenThereAreFiles(): void
+    {
+        $this->app['config']->set('breadcrumbs.files', [__DIR__.'/../routes/breadcrumbs.php']);
+
+        $this->assertInstanceOf(BreadcrumbsManager::class, $this->app->make('breadcrumbs.manager'));
+    }
+
+    public function testTheManagerIsNotResolvedWhenAConfiguredFileIsMissing(): void
+    {
+        $this->expectException(FileNotFoundException::class);
+
+        $this->app['config']->set('breadcrumbs.files', [__DIR__.'/non-existing.php']);
+
+        $this->app->make('breadcrumbs.manager');
+    }
+}
