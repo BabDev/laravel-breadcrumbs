713115dc32b365ef9406726519bd65e3e9e61135
Michael Babker <michael.babker@gmail.com>
2020-04-23 21:46:07 -0500

    Cleanup config

--- config/breadcrumbs.php
+++ config/breadcrumbs.php
@@ -10,14 +10,8 @@ return [
     | Choose a view to display when Breadcrumbs::render() is called.
     | Built in templates are:
     |
-    | - 'breadcrumbs::bootstrap4'  - Bootstrap 4
-    | - 'breadcrumbs::bootstrap3'  - Bootstrap 3
-    | - 'breadcrumbs::bootstrap2'  - Bootstrap 2
-    | - 'breadcrumbs::bulma'       - Bulma
-    | - 'breadcrumbs::foundation6' - Foundation 6
-    | - 'breadcrumbs::materialize' - Materialize
-    | - 'breadcrumbs::uikit'       - UIkit
-    | - 'breadcrumbs::json-ld'     - JSON-LD Structured Data
+    | - 'breadcrumbs::bootstrap4' - Bootstrap 4
+    | - 'breadcrumbs::json-ld'    - JSON-LD Structured Data
     |
     | Or a custom view, e.g. '_partials/breadcrumbs'.
     |
@@ -34,6 +28,8 @@ return [
     |
     | - base_path('routes/breadcrumbs.php')
     | - glob(base_path('breadcrumbs/*.php'))
+    |
+    | This may be a single file or an array of files.
     |
     */
 
