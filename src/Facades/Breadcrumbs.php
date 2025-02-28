0d1ecf86a0b99240c0cec9866d56db4aa33b2645
mbabker <368545+mbabker@users.noreply.github.com>
2025-02-28 01:56:36 +0000

    Update facade docblocks

--- src/Facades/Breadcrumbs.php
+++ src/Facades/Breadcrumbs.php
@@ -5,6 +5,19 @@ namespace BabDev\Breadcrumbs\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static void for(string $name, callable $callback)
+ * @method static bool exists(string|null $name = null)
+ * @method static \Illuminate\Support\Collection generate(string|null $name = null, mixed ...$params)
+ * @method static \Illuminate\Contracts\View\View view(string $view, string|null $name = null, mixed ...$params)
+ * @method static \Illuminate\Contracts\View\View render(string|null $name = null, mixed ...$params)
+ * @method static object|null current()
+ * @method static void setCurrentRoute(string $name, mixed ...$params)
+ * @method static void clearCurrentRoute()
+ * @method static void macro(string $name, object|callable $macro)
+ * @method static void mixin(object $mixin, bool $replace = true)
+ * @method static bool hasMacro(string $name)
+ * @method static void flushMacros()
+ *
  * @see \BabDev\Breadcrumbs\BreadcrumbsManager
  */
 class Breadcrumbs extends Facade
